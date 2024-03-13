6c2e6e9641838207c4e4a955b5923df9a487d964
Roman Arenas <roman.ea3232@gmail.com>
2024-03-13 17:27:26 -0300

    vista-login

---
+++ src/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <footer className='bg-slate-900 text-bisque p-4 flex flex-col gap-2 md:flex-row'>
+            <p className='text-white'>todos los derechos reservados</p>
+            <p className='text-white'>Superintendencia de Inteligencia Criminal</p>
+        </footer>
+    )
+}
