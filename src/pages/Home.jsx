52be80e3f9e094ae8e6c12fd8a15f3f05f7e4421
Roman Emiliano Arenas <roman.ea3232@gmail.com>
2024-04-25 17:17:31 -0300

    img-fondo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
-
 export const Home = () => {
   return (
     <>
-      <div  >Home</div>
+      <div className="home">
+        <div className="py-6 font-bold">
+          <h4 className="relative text-2xl text-black text-center">
+            Superintendencia
+          </h4>
+          <h4 className="relative text-2xl text-black text-center">
+            Inteligencia Criminal
+          </h4>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
